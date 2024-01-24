a7d158ea4e0d52f9195307088805a86a494844ad
GuilleCOzzi <gecozzi87@gmail.com>
2024-01-23 21:38:21 -0300

    Resuelto el primer punto (funcion factorear)

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,9 +6,19 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  //!Pasos
+  //! 1) crear un arreglo que contenga 1 que es el valor mas pequeño.
+  //! 2) usar Bucle para dividir y buscar los primos.
+  //! 3) usar if para comparar los numeros y pushearlos al arreglo.
+  let array1 = [1];
+  for (let i = 2; i <= num; i++){
+   while (num % i === 0) {
+    array1.push(i);
+    num = num / i;
+   } 
+  }
+  return array1
 }
-
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
